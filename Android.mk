d81fb9ca99cd75e92cc4126406ab4c62e70a0a15
mmqmemmm <rdgbnirr3418435@163.com>
2026-06-04 23:25:48 +0800

    mt6895-common: Add support for pearl
    Change-Id: I31138356b4a7db3b76eddafdc96714481ee381d9
    Signed-off-by: mmqmemmm <rdgbnirr3418435@163.com>

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter aristotle plato rembrandt rubens xaga,$(TARGET_DEVICE)),)
+ifneq ($(filter aristotle pearl plato rembrandt rubens xaga,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
